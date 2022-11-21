43a90379aeb8fb40438a26fe8984766e03319069
Atharva Wasekar <45729391+Electric-Dragon@users.noreply.github.com>
2022-11-21 14:34:40 +0530

    completed week 3

--- Week 3/contracts/ChainBattle.sol
+++ Week 3/contracts/ChainBattle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// 0x8F7B0d8aDc37E39D511d18c4c102A73f2f904Eb8
+// 0x5E8a8197ab3aF91344C95fCD3E5DbA39a5d54501
 
 pragma solidity ^0.8.0;
 
@@ -14,11 +14,18 @@ contract ChainBattles is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    mapping(uint256 => uint256) public tokenIdToLevels;
+    struct Stats { 
+        uint256 level;
+        uint256 speed;
+        uint256 strength;
+        uint256 life;
+    }
+
+    mapping(uint256 => Stats) public tokenIdToStats;
 
     constructor() ERC721 ("Chain Battles", "CBTLS"){}
 
-    function generateCharacter(uint256 tokenId) public returns(string memory){
+    function generateCharacter(uint256 tokenId) public view returns(string memory){
 
         bytes memory svg = abi.encodePacked(
             '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
@@ -37,11 +44,26 @@ contract ChainBattles is ERC721URIStorage {
     }
 
     function getLevels(uint256 tokenId) public view returns (string memory) {
-        uint256 levels = tokenIdToLevels[tokenId];
+        uint256 levels = tokenIdToStats[tokenId].level;
         return levels.toString();
     }
 
-    function getTokenURI(uint256 tokenId) public returns (string memory){
+    function getSpeed(uint256 tokenId) public view returns (string memory) {
+        uint256 speed = tokenIdToStats[tokenId].speed;
+        return speed.toString();
+    }
+
+    function getStrength(uint256 tokenId) public view returns (string memory) {
+        uint256 strength = tokenIdToStats[tokenId].strength;
+        return strength.toString();
+    }
+
+    function getLife(uint256 tokenId) public view returns (string memory) {
+        uint256 life = tokenIdToStats[tokenId].life;
+        return life.toString();
+    }
+
+    function getTokenURI(uint256 tokenId) public view returns (string memory){
         bytes memory dataURI = abi.encodePacked(
             '{',
                 '"name": "Chain Battles #', tokenId.toString(), '",',
@@ -61,15 +83,26 @@ contract ChainBattles is ERC721URIStorage {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        tokenIdToLevels[newItemId] = 0;
+        tokenIdToStats[newItemId] = Stats(1, 1, 1, 1);
         _setTokenURI(newItemId, getTokenURI(newItemId));
+    }
+
+    function random(uint number) private view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.timestamp,block.difficulty,  
+        msg.sender))) % number;
     }
 
     function train(uint256 tokenId) public {
         require(_exists(tokenId), "Please use an existing token");
         require(ownerOf(tokenId) == msg.sender, "You must own this token to train it");
-        uint256 currentLevel = tokenIdToLevels[tokenId];
-        tokenIdToLevels[tokenId] = currentLevel + 1;
+        uint256 currentLevel = tokenIdToStats[tokenId].level;
+        uint256 currentSpeed = tokenIdToStats[tokenId].speed;
+        uint256 currentStrength = tokenIdToStats[tokenId].strength;
+        uint256 currentLife = tokenIdToStats[tokenId].life;
+        tokenIdToStats[tokenId].level = currentLevel + 1;
+        tokenIdToStats[tokenId].speed = currentSpeed + random(3);
+        tokenIdToStats[tokenId].strength = currentStrength + random(3);
+        tokenIdToStats[tokenId].life = currentLife + random(3);
         _setTokenURI(tokenId, getTokenURI(tokenId));
     }
 
